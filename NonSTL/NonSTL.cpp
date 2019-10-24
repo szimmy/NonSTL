53880ab4467232737f9a4a3e20d804f88e547bc7
Sean <seanwzimmerman@gmail.com>
2019-10-23 22:23:28 -0400

    Update gitignore and add initial driver / vector code
    Added initial driver code and beginning implementation of non_stl::vector

---
+++ NonSTL/NonSTL.cpp
@@ -0,0 +1,13 @@
+﻿// NonSTL.cpp : Driver for testing the NonSTL implementations of data structures
+//
+
+#include "vector.h"
+
+#include <iostream>
+
+int main()
+{
+	non_stl::vector<int> vec(5);
+	std::cout << "Hello CMake." << std::endl;
+	return 0;
+}
---
+++ NonSTL/vector.h
@@ -0,0 +1,43 @@
+﻿// vector.h
+// A non-stl header only implementation of std::vector which attempts to adhere to the
+// standard interface as closely as possible.
+// Note, the standard is used for some components such as std::allocator and exceptions
+
+/*
+ * Vectors are sequence containers representing arrays that can change in size
+ */
+
+#pragma once
+
+#include <memory> // std::allocator
+
+namespace non_stl
+{
+	template <class T, class Alloc = std::allocator<T> >
+	class vector
+	{
+		// ---------------
+		// BEGIN INTERFACE
+		// ---------------
+	public:
+		// Default constructor
+		vector() = default;
+
+		explicit vector(size_t size);
+
+	private:
+		Alloc _alloc;
+		size_t _capacity;
+		size_t _size;
+		T* _data;
+	};
+
+	template <class T, class Alloc>
+	vector<T, Alloc>::vector(size_t size) :
+		_capacity(size),
+		_size(0),
+		_data(_alloc.allocate(size))
+	{
+
+	}
+}
